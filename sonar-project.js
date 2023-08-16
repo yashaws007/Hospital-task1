8c8e3d835751001fe0f84f12a9e75a869e75bc47
rajeshvr7 <125723807+rajeshvr7@users.noreply.github.com>
2023-08-16 10:33:19 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.178.3.8:9000/',
+  serverUrl: 'http://54.238.155.227:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - node',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_19fcdf6509efbfa386e0487dc927b22f537dc811',
+	    'sonar.login': 'squ_b47b73ef03c2051f1a9d3d3928c1c7a076e29f7b',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'sonar',
             'sonar.projectVersion':'1.0',
