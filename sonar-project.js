4cb0421f86fd059655b087f11de8095ffbcff900
yashaws007 <130629817+yashaws007@users.noreply.github.com>
2023-09-07 13:28:42 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.238.155.227:9000/',
+  serverUrl: 'http://54.166.246.111:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - node',
